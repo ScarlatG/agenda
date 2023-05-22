80dc30d6a517e7819a7cd7ede660cc03b0f50e37
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-05-22 22:25:50 +0200

    aggiunta della sicurezza

--- src/main/java/it/prova/agenda/web/api/UtenteController.java
+++ src/main/java/it/prova/agenda/web/api/UtenteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.agenda.model.Utente;
+import it.prova.agenda.security.dto.UtenteInfoJWTResponseDTO;
 import it.prova.agenda.service.utente.UtenteService;
 
 @RestController
